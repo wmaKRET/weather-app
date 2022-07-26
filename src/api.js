979dfa5441a13eb0abc08695648dece9d9d2e9c7
wmaKRET <wkret91.2@gmail.com>
2022-07-26 09:28:04 +0200

    create js file to store API data

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/"
+
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_GEO_API_KEY,
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+}
